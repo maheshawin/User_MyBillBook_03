2ce036609004108cf418f73a68cb43ac5e33ccfc
kondam <kondam@MAVCHN0522608.mavericsystems.lan>
2024-02-20 23:14:24 +0530

    commit 005

--- src/main/java/com/mybillbook/controllers/UserController.java
+++ src/main/java/com/mybillbook/controllers/UserController.java
@@ -19,7 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
 	UserService userService;
-
+	
+	
 	@PostMapping("/save")
 	public String saveUser(@RequestBody User user) {
 		log.info("inside saveUser");
